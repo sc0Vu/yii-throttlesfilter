6c9a8fdadb6b6d09d0101fde9c542f082a04aacd
sc0Vu <alk03073135@gmail.com>
2016-08-26 17:25:40 +0800

    first

---
+++ ThrottlesFilter.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * ThrottleFilter
+ *
+ * @author Peter Lai <alk03073135@gmail.com>
+ * @since 20160825
+ */
+class ThrottlesFilter extends CFilter
+{
+    /**
+     * requests throttle limit
+     * 
+     * @var integer
+     */
+    public $limit = 0;
+
+    /**
+     * limit seconds
+     * 
+     * @var integer
+     */
+    public $limitSeconds = 60;
+
+    /**
+     * lock seconds
+     * @var integer
+     */
+    public $lockSeconds = 60;
+    
+    /**
+     * Cache
+     * 
+     * @var CCache
+     */
+    protected $cache;
+
+    /**
+     * client ip
+     * 
+     * @var string
+     */
+    protected $ip;
+
+    /**
+     * preFilter
+     * 
+     * @param  CFilterChain $filterChain
+     * @return boolean
+     */
+    protected function preFilter($filterChain)
+    {
+        if ($this->limit === 0) {
+            return true;
+        }
+        if ($this->limitSeconds === 0) {
+            return false;
+        }
+
+        $this->cache = Yii::app()->cache;
+        $this->ip = Yii::app()->request->userHostAddress;
+        $cache = $this->cache;
+        $ip = $this->ip;
+        $now = time();
+        $diff = 0;
+
+        if (($lockTime = $cache->get($ip.'|lock')) !== false) {
+            if (($now - $lockTime) <= $this->lockSeconds) {
+                return false;
+            }
+            $cache->delete($ip.'|lock');
+        }
+        if ($cache->get($ip.'|count') === false) {
+            $cache->set($ip.'|count', 1);
+        }
+        if (($time = $cache->get($ip.'|time')) !== false) {
+            $diff = $now - $time;
+            if ($diff <= $this->limitSeconds) {
+                if ($cache->get($ip.'|count') > $this->limit) {
+                    $cache->set($ip.'|lock', $now);
+                    $cache->delete($ip.'|count');
+                    $cache->delete($ip.'|time');
+                    return false;
+                }
+            } else {
+                $cache->set($ip.'|count', 0);
+                $cache->set($ip.'|time', $now);
+            }
+        } else {
+            $this->cache->set($ip.'|time', $now);
+        }
+        return true;
+    }
+
+    /**
+     * postFilter
+     * 
+     * @param  CFilterChain $filterChain
+     * @return void
+     */
+    protected function postFilter($filterChain)
+    {
+        $cache = $this->cache;
+        $ip = $this->ip;
+        $count = $cache->get($ip.'|count');
+        $cache->set($ip.'|count', $count += 1);
+    }
+}
